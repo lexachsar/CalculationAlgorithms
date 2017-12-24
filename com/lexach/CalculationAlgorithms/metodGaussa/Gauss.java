075b86af9466fe9fdaf7503dcaded938ae4cc695
lexach <lexachsar@gmail.com>
2017-12-24 08:08:17 +0400

    Изменил архитектуру класса Gauss, предназначенного для решения СЛАУ Гаусса

--- com/lexach/CalculationAlgorithms/metodGaussa/Gauss.java
+++ com/lexach/CalculationAlgorithms/metodGaussa/Gauss.java
@@ -1,14 +1,89 @@
 package com.lexach.CalculationAlgorithms.metodGaussa;
 
-class Gauss{
-    public static void main(String args[]){
+class Gauss {
+    private static void oneAtMainDiagonaleMaker(int line, Sist system) {
+        double[][] a = system.getA();
+        double[] b = system.getB();
+        int n = system.getN();
+
+        for (int k = 0; k < n; k++) {
+            a[line][k] /= a[line][line];
+        }
+        b[line] /= a[line][line];
+    }
+
+    private static void zerosUnderMainDiagonaleMaker(int line, Sist system) {
+        double[][] a = system.getA();
+        double[] b = system.getB();
+        int n = system.getN();
+
+        for (int i = line + 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] += a[line][j] * (-a[i][line] / a[line][line]);
+                b[i] += b[line] * (-a[i][line] / b[line]);
+            }
+        }
+    }
+
+    private static void gaussPryamoy(Sist system) {
+        int n = system.getN();
+
+        for (int j = 0; j < n; j++) {
+            oneAtMainDiagonaleMaker(j, system);
+
+            zerosUnderMainDiagonaleMaker(j, system);
+        }
+    }
+
+    private static double[] gaussObratniy(Sist system) {
+        double[][] a = system.getA();
+        double[] b = system.getB();
+        int n = system.getN();
+
+        double[] result = new double[n];
+        double[] bvar = b.clone();
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                bvar[i] = bvar[i] - a[i][j] * result[j];
+            }
+            result[i] = (bvar[i]);
+        }
+        return result;
+    }
+
+    private static double[] masReverse(double[] mas){
+        for(int i = 0; i < mas.length / 2; i++) {
+            double temp = mas[i];
+            mas[i] = mas[mas.length - i - 1];
+            mas[mas.length - i - 1] = temp;
+        }
+
+        return mas;
+    }
+
+    private static double[] gauss(Sist system) {
+        gaussPryamoy(system);
+
+        double[] x = gaussObratniy(system);
+
+        return masReverse(x);
+    }
+
+    private static void masPrint(double[] mas) {
+        for (double ma : mas)
+            System.out.print(ma + " ");
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
         int n = 4;
         Sist MySist = new Sist(n);
         MySist.print();
 
-        MySist.gauss();
+        double[] x = gauss(MySist);
 
         MySist.print();
 
+        masPrint(x);
     }
 }
--- com/lexach/CalculationAlgorithms/metodGaussa/Sist.java
+++ com/lexach/CalculationAlgorithms/metodGaussa/Sist.java
@@ -2,29 +2,29 @@ package com.lexach.CalculationAlgorithms.metodGaussa;
 
 import java.util.Random;
 
-public class Sist{
-    protected static double[][] a;
-    protected static double[] b;
-    protected static int n;
+public class Sist {
+    private static double[][] a;
+    private static double[] b;
+    private static int n;
 
-    public Sist(int inN){
+    Sist(int inN) {
         n = inN;
         a = new double[n][n];
         b = new double[n];
 
         //создание системы линейных уравнений
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             Random r = new Random();
-            for(int j = 0; j < n; j++){
+            for (int j = 0; j < n; j++) {
                 a[i][j] = (double) ((int) (r.nextDouble() * 1000)) / 10;
             }
             b[i] = (double) ((int) (r.nextDouble() * 1000)) / 10;
         }
     }
 
-    public static void print(){
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
+    public void print() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 System.out.print(a[i][j]);
                 System.out.print("\t");
             }
@@ -34,53 +34,16 @@ public class Sist{
         System.out.println();
     }
 
-    public static void oneAtMainDiagonaleMaker(int line){
-        for(int k = 0; k < n; k++){
-            a[line][k] /= a[line][line];
-        }
-        b[line] /= a[line][line];
+    public double[][] getA() {
+        return a;
     }
 
-    public static void zerosUnderMainDiagonaleMaker(int line){
-        for(int i = line + 1; i < n; i++){
-            for(int j = 0; j < n; j++){
-                a[i][j] += a[line][j] * ( - a[i][line] / a[line][line] );
-                b[i] += b[line] * ( -a[i][line] / b[line]);
-            }
-        }
+    public double[] getB() {
+        return b;
     }
 
-    public static void gaussPryamoy(){
-        for(int j = 0; j < n; j++){
-            oneAtMainDiagonaleMaker(j);
-
-            zerosUnderMainDiagonaleMaker(j);
-        }
-    }
-
-    public static double[] gaussObratniy(){
-        double[] result = new double[n];
-        double[] bvar = b.clone();
-        for(int i = n - 1; i >= 0; i-- ){
-            for(int j = i + 1; j < n; j++){
-                bvar[i] = bvar[i] - a[i][j] * result[j];
-            }
-            result[i] =( bvar[i] );
-        }
-        return result;
-    }
-
-    public static void masPrint(double[] mas){
-        for(int i = 0; i < n; i++)
-            System.out.print(mas[i] + " ");
-        System.out.println();
-        System.out.println();
-    }
-
-    public static void gauss(){
-        gaussPryamoy();
-
-        masPrint( gaussObratniy() );
+    public int getN() {
+        return n;
     }
 
 }
